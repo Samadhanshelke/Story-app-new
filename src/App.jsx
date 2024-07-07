20c032e478442908e3d2b5963db813be1bb614fe
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-07-07 16:38:50 +0530

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,99 @@
-import { useState, useRef, useEffect } from 'react';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 const reels = [
-  'Reel Content 1: This is the first reel.Mollit dolore excepteur velit id pariatur elit. Nostrud commodo eu aliquip eiusmod officia elit commodo exercitation. Minim eu ullamco elit elit fugiat excepteur id occaecat commodo enim culpa. Velit enim commodo quis nisi veniam est labore exercitation qui enim veniam mollit magna nisi. In sunt irure proident do.Exercitation aliquip eu Lorem culpa. Pariatur velit aute consectetur eiusmod aliqua excepteur ullamco aliqua officia et ex ex exercitation. Nisi ipsum sunt occaecat ullamco eu aute dolore cillum. Est et dolore officia cupidatat qui proident commodo. Dolore dolore occaecat tempor do non veniam cillum enim excepteur. Sit aliqua consequat consequat id est est exercitation quis.Excepteur fugiat est magna ipsum anim magna consectetur mollit commodo. Ex pariatur nostrud ullamco in labore id consequat consequat. Amet reprehenderit irure velit aute.Elit dolore laboris enim eu pariatur adipisicing excepteur eu. Elit cupidatat mollit magna cillum quis. Commodo est nulla velit ad duis proident non incididunt enim est.Cupidatat adipisicing sunt nisi magna esse laboris qui aliquip aliquip sunt ad. Commodo sunt nisi ea qui nisi veniam. Occaecat consectetur cillum sit cupidatat do excepteur consequat nulla nisi exercitation. Laborum ipsum mollit tempor dolore nostrud Lorem laboris laborum adipisicing occaecat voluptate. Excepteur nulla consectetur eu Lorem elit esse aliquip. Laboris tempor amet adipisicing pariatur ex aute ullamco deserunt dolore. Ipsum ex esse labore sint reprehenderit elit.',
-  'Reel Content 2: This is the second reel.',
-  'Reel Content 3: This is the third reel.',
+  "Reel Content 1: This is the first reel. Mollit dolore excepteur velit id pariatur elit. Nostrud commodo eu aliquip eiusmod officia elit commodo exercitation. Minim eu ullamco elit elit fugiat excepteur id occaecat commodo enim culpa. Velit enim commodo quis nisi veniam est labore exercitation qui enim veniam mollit magna nisi. In sunt irure proident do. Exercitation aliquip eu Lorem culpa. Pariatur velit aute consectetur eiusmod aliqua excepteur ullamco aliqua officia et ex ex exercitation. Nisi ipsum sunt occaecat ullamco eu aute dolore cillum. Est et dolore officia cupidatat qui proident commodo. Dolore dolore occaecat tempor do non veniam cillum enim excepteur. Sit aliqua consequat consequat id est est exercitation quis. Excepteur fugiat est magna ipsum anim magna consectetur mollit commodo. Ex pariatur nostrud ullamco in labore id consequat consequat. Amet reprehenderit irure velit aute. Elit dolore laboris enim eu pariatur adipisicing excepteur eu. Elit cupidatat mollit magna cillum quis. Commodo est nulla velit ad duis proident non incididunt enim est. Cupidatat adipisicing sunt nisi magna esse laboris qui aliquip aliquip sunt ad. Commodo sunt nisi ea qui nisi veniam. Occaecat consectetur cillum sit cupidatat do excepteur consequat nulla nisi exercitation. Laborum ipsum mollit tempor dolore nostrud Lorem laboris laborum adipisicing occaecat voluptate. Excepteur nulla consectetur eu Lorem elit esse aliquip. Laboris tempor amet adipisicing pariatur ex aute ullamco deserunt dolore. Ipsum ex esse labore sint reprehenderit elit.",
+  "Reel Content 2: This is the second reel.",
+  "Reel Content 3: This is the third reel.",
 ];
 
 const App = () => {
   const [index, setIndex] = useState(0);
-  const startY = useRef(0);
-  const endY = useRef(0);
-  const isDragging = useRef(false);
-  const reelRefs = useRef([]);
+  const startYPosition = useRef(0); // To store the initial Y position
+  const previousYPosition = useRef(0); // To store the previous Y position during the move
+  const direction = useRef("");
+  const ReelsRef = useRef([]);
 
   useEffect(() => {
-    reelRefs.current = reelRefs.current.slice(0, reels.length);
+    ReelsRef.current = ReelsRef.current.slice(0, reels.length);
   }, []);
 
-  const handleStart = (y) => {
-    startY.current = y;
-    isDragging.current = true;
-  };
+  const handleMouseMove = (e) => {
+    const y = e.clientY;
+ 
+   
 
-  const handleMove = (y) => {
-    if (isDragging.current) {
-      endY.current = y;
+
+    if (y < previousYPosition.current) {
+      direction.current = "up";
+    } else if (y > previousYPosition.current) {
+      direction.current = "down";
     }
+    previousYPosition.current = y;
   };
 
-  const handleEnd = () => {
-    if (isDragging.current) {
-      const delta = startY.current - endY.current;
-      if (delta > 50) {
-        if (index === 0 && reelRefs.current[index].scrollHeight > reelRefs.current[index].clientHeight) {
-          const currentScrollPosition = reelRefs.current[index].scrollTop;
-          const scrollBottom = reelRefs.current[index].scrollHeight - reelRefs.current[index].clientHeight;
-          if (currentScrollPosition >= scrollBottom - 1) { 
+  const handleMouseDown = (e) => {
+    const y = e.clientY;
+    startYPosition.current = y;
+    previousYPosition.current = y;
+  };
+
+  const getDirection = () => {
+    const reel = ReelsRef.current[index];
+    if (ReelsRef.current) {
+      const hasScrollbar = reel.scrollHeight > reel.clientHeight;
+      // const hasScrollbar = false;
+      const currentScrollPosition = reel.scrollTop;
+      const scrollBottom = reel.scrollHeight - reel.clientHeight;
+
+    console.log('scrollTop',currentScrollPosition,scrollBottom)
+
+     const resetScroll = ()=>{
+      if (reel) {
+        reel.scrollTop = 0; 
+      }
+     }
+      if (direction.current === "up") {
+        if (hasScrollbar) {
+          if (currentScrollPosition >= scrollBottom - 5) {
             setIndex((prevIndex) => (prevIndex < reels.length - 1 ? prevIndex + 1 : prevIndex));
+            resetScroll()
           }
         } else {
           setIndex((prevIndex) => (prevIndex < reels.length - 1 ? prevIndex + 1 : prevIndex));
+          resetScroll()
         }
-      } else if (delta < -50) {
+      } else if (direction.current === "down") {
         setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+        resetScroll()
       }
     }
-    isDragging.current = false;
   };
 
-  const handleMouseDown = (e) => handleStart(e.clientY);
-  const handleMouseMove = (e) => handleMove(e.clientY);
-  const handleMouseUp = handleEnd;
 
-  const handleTouchStart = (e) => handleStart(e.touches[0].clientY);
-  const handleTouchMove = (e) => handleMove(e.touches[0].clientY);
-  const handleTouchEnd = handleEnd;
+
+  const handleMouseUp = getDirection;
 
   return (
-    <div
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-      className="h-[100vh] overflow-hidden">
-      {reels.map((item, idx) => (
-        <div
-          key={item}
-          ref={(el) => (reelRefs.current[idx] = el)}
-          className={`h-[100vh] border-2 border-red-600 overflow-auto ${idx === index ? 'block' : 'hidden'}`}>
-          <span className='text-6xl'>{item}</span>
-        </div>
-      ))}
+    <div className="h-[100vh]">
+      {reels.map((item, i) => {
+        return (
+          <div
+            key={item}
+            ref={(el) => (ReelsRef.current[i] = el)}
+            onMouseMove={handleMouseMove}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            className={` h-full text-6xl overflow-auto select-none  ${index === i ? "flex" : "hidden"}`}
+               
+          >
+          <p>
+
+            {item}
+          </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
